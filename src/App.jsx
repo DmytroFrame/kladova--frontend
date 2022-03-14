8b4c839689710a4e9790b4bdd8cbf03849784dc3
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2022-03-14 21:36:43 +0200

    edit application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
+// import axios from 'axios'
 import './App.scss'
-import axios from 'axios'
-import { Route,  Routes } from 'react-router-dom'
-import Music from './pages/Music'
 import Navbar from './components/Navbar/Navbar'
-import Home from './pages/Home'
-import Course from './pages/Course'
-import People from './pages/People'
-import Login from './pages/Login/Login'
+import Copyright from './components/Copyright'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
 
 
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Navbar/>}>
-          <Route index element={<Home/>}/>
-          <Route path='course' element={<Course/>}/>
-          <Route path='music' element={<Music/>}/>
-          <Route path='people' element={<People/>}/>
-          {/* <Route path='*'></Route> */}
-        </Route>
-        <Route path='login' element={<Login/>} /> 
-      </Routes>
-    </div>
+    <>
+      <header>
+        <Navbar/>
+      </header>
+
+      <main>
+        <Outlet/>
+      </main>
+
+      {/* <footer>
+        <Copyright/>
+      </footer> */}
+    </>
   )
 }
 
