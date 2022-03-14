18a1e469185532fe0666c180340f053423830229
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2022-03-14 17:17:40 +0200

    create pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.scss'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <div className='home__content'>
+        <div className='b'></div>
+        <div className='c'></div>
+        <div className='d'></div>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/Music.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Music.scss'
+
+export default function Music() {
+  return (
+    <div className='music'>
+        <h1>Coming soon..</h1>
+    </div>
+  )
+}
