de683f100165873661b8077da62a353775c3b90c
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2022-03-14 20:55:22 +0200

    made Copyright component

---
+++ src/components/Copyright/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Copyright() {
+  return (
+      <div className='copyright'>
+        <h3>© Kladova 2022.</h3>
+        <p>Author <a>DmytroFrame</a></p>
+      </div>
+  )
+}
