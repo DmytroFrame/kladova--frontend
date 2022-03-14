73e0e8d732b3b22217434a3faed0067be1c6a584
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2022-03-14 21:18:48 +0200

    delete Outlet made only one component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,34 +1,30 @@
 import React from 'react'
 import './Navbar.scss'
 import logo from './logo.svg'
-import { Outlet, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div>
-        <nav className='navbar'>
-            <div className='navbar__content'>
-                <div className='navbar__logo'>
-                    <Link to="/">
-                        <img src={logo} height="60px" alt='Kladova logo'/>    
-                    </Link>   
-                </div>
-
+    <div className='navbar'>
+        <div className='navbar__content'>
+            <div className='navbar__logo'>
+                <Link to="/">
+                    <img src={logo} height="60px" alt='Kladova logo'/>    
+                </Link>   
+            </div>
+            <nav>
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/course">Course</Link></li>
                     <li><Link to="/music">Music</Link></li>
                     <li><Link to="/people">People</Link></li>
                 </ul>
-
-                <div className='navbar__panel'>
-                    <p>DmytroFrame</p>
-                </div>
-                <img className='navbar__user_icon' src="https://avatars.githubusercontent.com/u/34944544?v=4" alt="user icon" width="45px" />
-
+            </nav>
+            <div className='navbar__panel'>
+                <p>DmytroFrame</p>
             </div>
-        </nav>
-        <Outlet />
+            <img className='navbar__user_icon' src="https://avatars.githubusercontent.com/u/34944544?v=4" alt="user icon" width="45px" />
+        </div>
     </div>
   )
 }
