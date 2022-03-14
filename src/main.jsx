4bc9717c0b314da3c963ffc8ee95dbdd346fc684
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2022-03-14 21:41:35 +0200

    edit application structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import Course from './pages/Course'
+import Music from './pages/Music'
+import People from './pages/People'
+import Login from './pages/Login/Login'
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path='/' element={<App/>}>
+          <Route index element={<Home/>}/>
+          <Route path='course' element={<Course/>}/>
+          <Route path='music' element={<Music/>}/>
+          <Route path='people' element={<People/>}/>
+
+          {/* <Route path='*'></Route> */}
+
+        </Route>
+        <Route path='login' element={<Login/>} /> 
+
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
