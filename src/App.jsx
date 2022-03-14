9e289cb36b5d0897f5887a90d9fc154494eb59fa
Dmytro Kostyshyn <dima.kostishen@gmail.com>
2022-03-14 17:20:01 +0200

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from 'react'
 import './App.scss'
 import axios from 'axios'
-import {} from 'react-router-dom'
+import { Route,  Routes } from 'react-router-dom'
+import Music from './pages/Music'
+import Navbar from './components/Navbar/Navbar'
+import Home from './pages/Home'
+import Course from './pages/Course'
+import People from './pages/People'
+import Login from './pages/Login/Login'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +17,16 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Navbar/>}>
+          <Route index element={<Home/>}/>
+          <Route path='course' element={<Course/>}/>
+          <Route path='music' element={<Music/>}/>
+          <Route path='people' element={<People/>}/>
+          {/* <Route path='*'></Route> */}
+        </Route>
+        <Route path='login' element={<Login/>} /> 
+      </Routes>
     </div>
   )
 }
